f627d132d896461ced08fd0159812dc0f817001d
rajkumar11820 <sameersuraj132@gmail.com>
2025-06-15 16:59:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import asyncio
+import os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello from Railway!")
+
+async def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("Bot is running via polling...")
+    await app.run_polling()
+
+if __name__ == "__main__":
+    asyncio.run(main())
